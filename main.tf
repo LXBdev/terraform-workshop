bc574194961ceff7624ade0227b66c1587e8f651
Alexander Bender <Alexander.Bender@aitgmbh.de>
2019-09-06 14:08:16 +0200

    random

--- main.tf
+++ main.tf
@@ -4,8 +4,16 @@ resource "azurerm_resource_group" "lab1" {
   tags = "${var.tags}"
 }
 
+resource "random_string" "random" {
+  length = 8
+  special = false
+  upper = false
+  lower = true
+}
+
+
 resource "azurerm_storage_account" "labstorage" {
-    name    = "testaitterraformlab"
+    name    = "testaitterrafor${random_string.random.result}"
     resource_group_name = "${azurerm_resource_group.lab1.name}"
     account_tier = "Standard"
     account_replication_type = "LRS"
