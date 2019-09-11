494e15410c2d790b92accd2360a6ddbd0e035a2a
Alexander Bender <Alexander.Bender@aitgmbh.de>
2019-09-11 21:14:48 +0200

    application insights tracking

--- main.tf
+++ main.tf
@@ -59,4 +59,7 @@ resource "azurerm_app_service" "webapp" {
   resource_group_name = "${azurerm_resource_group.lab1.name}"
   location            = "${element(azurerm_app_service_plan.webapp-plan.*.location, count.index)}"
   app_service_plan_id = "${element(azurerm_app_service_plan.webapp-plan.*.id, count.index)}"
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.appInsights.instrumentation_key}"
+  }
 }
